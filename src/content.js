821827f87c9dd63c88305939c8787a49b2d3a9bf
John Elliott <160415536+Jakelliott98@users.noreply.github.com>
2025-01-29 17:45:12 +0000

    Cleaning up Js and adding CSS
    Cleaning up some Js with better variable names and ID's.
    
    Playing around with CSS still getting used to using JS and CSS together

--- src/content.js
+++ src/content.js
@@ -3,93 +3,98 @@ import bannerImage from './bannerImg.png';
 
 export function createContent(){
 
-const mainBody = document.getElementById('content');
+    const mainBody = document.getElementById('content');
 
-function createBanner () {
+    function createBanner () {
 
-const bannerDiv = document.createElement('div');
-const button = document.createElement('button');
+        const bannerDiv = document.createElement('div');
+        const button = document.createElement('button');
 
-button.innerHTML = 'Book A Table';
+        button.innerHTML = 'Book A Table';
 
-bannerDiv.id = 'bannerDiv';
-button.className = 'button';
+        bannerDiv.id = 'bannerDiv';
+        button.className = 'button';
+        button.id = 'bannerCTA';
 
-mainBody.appendChild(bannerDiv);
-bannerDiv.appendChild(button);
-
-};
-
-function createInfoContent(){
-
-const secondDiv = document.createElement('div');
-mainBody.appendChild(secondDiv);
-
-function createLeftText () {
-
-const textDiv = document.createElement('div');
-const subTitle = document.createElement('h3');
-const paragraph = document.createElement('p');
-
-textDiv.id = 'leftContainer';
-subTitle.id = 'leftHeading';
-paragraph.id = 'leftText';
-
-subTitle.innerHTML = 'The Ivy In The Park, Canary Wharf';
-paragraph.innerHTML = `The Ivy in the Park View brings friendly, sophisticated all-day dining to the heart of Canary Wharf. From modern British classics to Asian-inspired cuisine and fantastic vegan and vegetarian dishes, you’ll find something for everyone to love. Located in one of Canary Wharf’s most sustainable buildings, the restaurant is set across two floors. As well as the main dining area and bar, there are two outdoor terraces overlooking a vibrant green space, and offering those working, living and visiting the area a perfect spot to relax and unwind in the warmer months.`
-
-secondDiv.appendChild(textDiv);
-textDiv.appendChild(subTitle);
-textDiv.appendChild(paragraph);
-
-};
-
-function createRightContent(){
-
-    const mainDiv = document.createElement('div');
-    const title = document.createElement('h3');
-    const menuDiv = document.createElement('div');
-    const button = document.createElement('button');
-
-    button.innerHTML = 'View All Menus';
-    button.className = 'button';
-    
-    function createComponent(title, text){
-
-        const contentDiv = document.createElement('div');
-        const titleElement = document.createElement('h5');
-        const textElement = document.createElement('p');
-
-        contentDiv.className = 'menuConent';
-        titleElement.className = 'menuTitle';
-        textElement.className = 'menuText';
-
-        titleElement.innerHTML = title;
-        textElement.innerHTML = text;
-
-        secondDiv.appendChild(contentDiv);
-        contentDiv.appendChild(titleElement);
-        contentDiv.appendChild(textElement);
+        mainBody.appendChild(bannerDiv);
+        bannerDiv.appendChild(button);
 
     };
 
-    mainBody.appendChild(mainDiv);
-    mainDiv.appendChild(title);
-    mainDiv.appendChild(menuDiv);
-    mainDiv.appendChild(button);
+    function createInfoContent(){
 
-    createComponent('A La Carte', 'Available Everyday');
-    createComponent('$19.50 Two-Course Menu', 'Available Monday - Friday, 11:30am - 6:30pm');
-    createComponent('Breakfast', 'Available Everyday');
+        const restarauntInfo = document.createElement('section');
+        restarauntInfo.id = 'restarauntInfo';
+        mainBody.appendChild(restarauntInfo);
 
-};
+        function createRestarauntText () {
 
-createLeftText();
-createRightContent();
+            const info = document.createElement('section');
+            const heading = document.createElement('h3');
+            const paragraph = document.createElement('p');
 
-};
+            info.id = 'textInfo'
 
-createBanner();
-createInfoContent();
+            heading.innerHTML = 'The Ivy In The Park, Canary Wharf';
+            paragraph.innerHTML = `The Ivy in the Park View brings friendly, sophisticated all-day dining to the heart of Canary Wharf. From modern British classics to Asian-inspired cuisine and fantastic vegan and vegetarian dishes, you’ll find something for everyone to love. Located in one of Canary Wharf’s most sustainable buildings, the restaurant is set across two floors. As well as the main dining area and bar, there are two outdoor terraces overlooking a vibrant green space, and offering those working, living and visiting the area a perfect spot to relax and unwind in the warmer months.`
+
+            restarauntInfo.appendChild(info);
+            info.appendChild(heading);
+            info.appendChild(paragraph);
+
+        };
+
+        function createMenuListing(){
+
+            const menus = document.createElement('section');
+            const heading = document.createElement('h3');
+            const menuListings = document.createElement('section');
+            const button = document.createElement('button');
+
+            menus.id = 'menuContainer';
+            menuListings.id = 'menuListings';
+
+            heading.innerHTML = 'Menus';
+            button.innerHTML = 'View All Menus';
+            button.className = 'button';
+        
+            function createComponent(title, text){
+
+                const contentDiv = document.createElement('div');
+                const titleElement = document.createElement('h5');
+                const textElement = document.createElement('p');
+
+                contentDiv.className = 'menuConent';
+                titleElement.className = 'menuTitle';
+                textElement.className = 'menuText';
+
+                titleElement.innerHTML = title;
+                textElement.innerHTML = text;
+
+                restarauntInfo.appendChild(menus);
+                menuListings.appendChild(contentDiv);
+                contentDiv.appendChild(titleElement);
+                contentDiv.appendChild(textElement);
+
+            };
+
+            mainBody.appendChild(menus);
+            menus.appendChild(heading);
+            menus.appendChild(menuListings);
+            menus.appendChild(button);
+
+            createComponent('A La Carte', 'Available Everyday');
+            createComponent('$19.50 Two-Course Menu', 'Available Monday - Friday, 11:30am - 6:30pm');
+            createComponent('Breakfast', 'Available Everyday');
+
+        };
+
+    createRestarauntText();
+    createMenuListing();
+
+    };
+
+    createBanner();
+    createInfoContent();
 
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './styles.css';
 import { createContent } from "./content.js";
+import './styles.css';
 
 createContent();
